12dc4175db55673e4c074453572016498ac4a743
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-07-30 02:20:58 +0430

    test cases added

--- energycalc/__main__.py
+++ energycalc/__main__.py
@@ -1,6 +1,14 @@
 from .energy import *
-
+import sys
+import doctest
 if __name__=="__main__":
-    print_sign()
-    print("Total :"+get_input())
-    input()
+    args=sys.argv
+    if len(args)>1:
+        if args[1].upper()=="TEST":
+            doctest.testfile("test.py",verbose=True)
+        else:
+            print("Bad Input")
+    else:
+        print_sign()
+        print("Total :"+get_input())
+        input()
---
+++ energycalc/test.py
@@ -0,0 +1,28 @@
+'''
+>>> from energycalc import *
+>>> line(5)
+'*****'
+>>> line(5,"&")
+'&&&&&'
+>>> convert(400)
+'400.0 wh'
+>>> convert(145400)
+'145.4 Kwh'
+>>> check_constraint(2)
+2
+>>> check_constraint(400)
+24
+>>> check_constraint(400,mode=2)
+365
+>>> handle_input("34")
+34
+>>> handle_input('asdasd')
+0
+>>> seperator("2,3,4,5,6")
+['2', 3, 4, 5, 6]
+>>> seperator("2;3;4;5;6",char=";")
+['2', 3, 4, 5, 6]
+>>> find_ref()
+'NOFILE'
+
+'''
